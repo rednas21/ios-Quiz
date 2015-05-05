4755fc9cae0d9b6376d9e58ac0869d6b66d8f0af
Sander Peerna <sanderpeerna@gmail.com>
2015-05-04 17:01:19 -0700

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Sander Peerna on 5/4/15.
+//  Copyright (c) 2015 Sander Peerna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
