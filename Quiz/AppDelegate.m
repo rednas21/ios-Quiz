8c7b1c228d081763d2633c3ac2672076d322c9bd
Sander Peerna <sanderpeerna@gmail.com>
2015-05-22 16:45:04 -0700

    First commit, though this project is almost done.

--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -7,16 +7,19 @@
 //
 
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
-@end
+#import "ViewController.h"
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
+//    ViewController *quizVC = [[ViewController alloc] init];
+//    self.window.rootViewController = quizVC;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -10,18 +10,55 @@
 
 @interface ViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view from its nib.
+    
+    if (self)
+    {
+        //Create two arrays filled with questions and answers
+        self.questions = [[NSArray alloc] init];
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        
+        self.answers = [[NSArray alloc] init];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)showQuestion:(id)sender
+{
+    self.currentQuestionIndex++;
+    if (self.currentQuestionIndex == [self.questions count])
+    {
+        self.currentQuestionIndex = 0;
+    }
+    
+    NSString *question = self.questions[self.currentQuestionIndex];
+    self.questionLabel.text = question;
+    self.answerLabel.text = @"???";
+}
+
+-(IBAction)showAnswer:(id)sender
+{
+    NSString *answer = self.answers[self.currentQuestionIndex];
+    self.answerLabel.text = answer;
 }
 
 @end
